0cf7f52f8f5e3e58e309e3f5a96be1f2bf52d5f9
Damian Koch <Damian Koch@localhost>
2018-11-12 13:13:33 +0100

    added note file and enemy group

---
+++ NOTES.gd
@@ -0,0 +1,8 @@
+#add bee
+
+#add enemy group, add enemies on ready
+#bee on trigger: enemies invis, disable collision shapes
+#bee alt(if possible): enemies move away from bee, return to closest path point after t seconds
+
+#bg parallaxing
+
