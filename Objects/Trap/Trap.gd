1ffc68ab8774d99b9e0df6dfc72c5ccea70dbff0
Damian Koch <Damian Koch@localhost>
2018-11-12 11:53:15 +0100

    fixed player disappearing when killed by trap, started working on boxes destroyed by traps respawning on player respawn

--- Objects/Trap/Trap.gd
+++ Objects/Trap/Trap.gd
@@ -1,12 +1,17 @@
 extends Area2D
 
 var triggered = false
-onready var triggeredBody = $triggeredTrap/CollisionShape2D
+onready var snappedBody = $triggeredTrap/CollisionShape2D
 onready var player = get_parent().find_node("Onion")
 var triggerCD = 0
+onready var boxPacked = preload("res://Objects/Box/Box.tscn")
+var triggerStartpos
+
+
+
 
 func _ready():
-	triggeredBody.disabled = true
+	snappedBody.disabled = true
 	player.connect("loseHp", self, "open")
 	
 
@@ -16,14 +21,23 @@ func _physics_process(delta):
 func _on_Trap_body_entered(body):
 	if triggered == false && triggerCD == 0:
 		if body.name != "StaticBody2D" && body.name != "TileMap":
-			triggered = true
-			triggeredBody.disabled = false
-			$AnimationPlayer.play("Snap")
 			if body.name == "Onion":
 				body.health -= 1
+			else:
+				if body.get_filename() == boxPacked.get_path():
+					triggerStartpos = body.startpos
+				body.get_parent().remove_child(body)
+				triggered = true
+				snappedBody.disabled = false
+				$AnimationPlayer.play("Snap")
+			
 
 func open():
 	triggerCD = 1
+	if triggerStartpos != null:
+		var box = boxPacked.instance()
+		box.position = triggerStartpos
+		triggerStartpos = null
 	$AnimationPlayer.play("Open")
-	triggeredBody.disabled = true
+	snappedBody.disabled = true
 	triggered = false
