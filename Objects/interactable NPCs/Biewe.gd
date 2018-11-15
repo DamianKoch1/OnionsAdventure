64abeca9c0124960be3330136bf6ce632c9242d1
Damian Koch <Damian Koch@localhost>
2018-11-15 17:52:21 +0100

    implemented biewe (prototype, nonfunctional)

--- Objects/interactable NPCs/Biewe.gd
+++ Objects/interactable NPCs/Biewe.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
+export var bieweCooldown = 8
+export var bieweRange = 600
+var cd = 0
+var enemies
+
+func _ready():
+	enemies = get_tree().get_nodes_in_group("Enemies")
+
+func _physics_process(delta):
+	cd = max(cd - delta, 0)
 
 func _on_Area2D_body_entered(body):
-	if body.name == "Onion":
-		#make nodes in enemy group disappear/flee for t s
-		pass
+	if body.name == "Onion" && cd == 0:
+		for i in range(0, enemies.size()):
+			if enemies[i].global_position.distance_to(global_position) <= bieweRange:
+				enemies[i].flee()
