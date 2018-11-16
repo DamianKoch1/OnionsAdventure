2cb00c91ebc5023823c48a64023229c3e5dae6ae
Damian Koch <Damian Koch@localhost>
2018-11-16 15:16:03 +0100

    commented some code

--- Objects/Checkpoint/Checkpoint.gd
+++ Objects/Checkpoint/Checkpoint.gd
@@ -4,6 +4,7 @@ onready var spawnpoint = get_parent().find_node("Spawnpoint")
 
 func _on_Checkpoint_body_entered(body):
 	if body.name == "Onion":
+		#move spawnpoint to checkpoint position
 		spawnpoint.global_position = global_position
 		body.health = global.maxHealth
 		queue_free()
