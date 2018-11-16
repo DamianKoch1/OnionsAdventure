847354460688398c7bf62691d3f084c9f70d7416
Damian Koch <Damian Koch@localhost>
2018-11-16 10:23:59 +0100

    finished biewe prototype

--- Enemies/FollowingEnemy.gd
+++ Enemies/FollowingEnemy.gd
@@ -2,36 +2,51 @@ extends Node2D
 
 var player
 var startpos
-onready var damageCD = 0
 onready var following = false
 onready var enemy = $Enemy
 export var speed = 2
 export var loseAggroDistance = 550
 var motion = Vector2()
 export var gravity = 12
+onready var hidden = false
+onready var hideTimer = 0
 
 func _ready():
 	startpos = enemy.global_position
 	add_to_group("Enemies")
 
 func flee(duration):
-	
+	hidden = true
+	hideTimer = duration
+	#$Enemy/Sprites.hide()
+	$Enemy/CollisionShape2D.disabled = true
+	$Enemy/Area2D/CollisionShape2D.disabled = true
+	$VisionRange/CollisionShape2D.disabled = true
 
+func reappear():
+	hidden = false
+	#$Enemy/Sprites.show()
+	$Enemy/CollisionShape2D.disabled = false
+	$Enemy/Area2D/CollisionShape2D.disabled = false
+	$VisionRange/CollisionShape2D.disabled = false
+	
 func _process(delta):
-	if damageCD != 0:
-		damageCD = max(damageCD - delta, 0)
-	motion.y += gravity
-	if following == true:
-		motion.x = (player.global_position.x - $Enemy.global_position.x)*speed
-		if startpos.distance_to(player.global_position) > loseAggroDistance:
-			following = false
-	else:
-		motion.x = startpos.x - $Enemy.global_position.x
-	motion = $Enemy.move_and_slide(motion)
+	hideTimer = max(hideTimer-delta, 0)
+	if hidden == true && hideTimer == 0:
+		reappear()
+	if hidden == false:
+		motion.y += gravity
+		if following == true:
+			motion.x = (player.global_position.x - $Enemy.global_position.x)*speed
+			if startpos.distance_to(player.global_position) > loseAggroDistance:
+				following = false
+		else:
+			motion.x = startpos.x - $Enemy.global_position.x
+		motion = $Enemy.move_and_slide(motion)
+		
 func _on_Area2D_body_entered(body):
-	if body.name == "Onion" && damageCD == 0:
+	if body.name == "Onion":
 		body.health -= 1
-		damageCD = 1
 
 
 func _on_VisionRange_body_entered(body):
