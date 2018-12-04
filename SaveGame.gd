9c385d2eb4489b89c4d065e794d0f0bb978752ce
Damian Koch <Damian Koch@localhost>
2018-12-04 16:36:03 +0100

    can now save and load correct level and difficulty at latest checkpoint
    - working on setting player position and npc saved counter

--- SaveGame.gd
+++ SaveGame.gd
@@ -1,23 +1,30 @@
 extends Node
 
-var savePath = "user://save.cfg" 
+var savePath 
 var saveFile
 
 var latestCheckpoint
 
-
-
 func _ready():
+	savePath = "user://save.cfg" 
 	saveFile = ConfigFile.new()
 	
-	
+
 func saveGame():
-	saveFile.set_value("Save state", "latest level id", global.currLevelId) 
-	saveFile.set_value("Save state", "latest checkpoint", latestCheckpoint) 
-	saveFile.set_value("Save state", "difficulty", global.diff) 
-	saveFile.set_value("Save state", "NPCs saved", global.player.NPCsavedCount) 
+	saveFile.set_value("SaveState", "latestLevelId", global.currLevelId) 
+	saveFile.set_value("SaveState", "spawnPosX", latestCheckpoint.position.x) 
+	saveFile.set_value("SaveState", "spawnPosY", latestCheckpoint.position.y) 
+	saveFile.set_value("SaveState", "difficulty", global.diff) 
+	saveFile.set_value("SaveState", "NPCsSaved", global.player.NPCsavedCount) 
 	saveFile.save(savePath)
 
 func loadGame():
-	pass
+	saveFile.load(savePath)
+	var levelToLoadId = saveFile.get_value("SaveState", "latestLevelId") 
+	global.diff = saveFile.get_value("SaveState", "difficulty") 
+	var path = "Levels/Level " + str(levelToLoadId) + ".tscn"
+	get_tree().change_scene(path)
+	#global.player.NPCsavedCount = saveFile.get_value("Save state", "NPCs saved") 
+	#global.player.position.x = saveFile.get_value("Save state", "spawnPosX")
+	#global.player.position.y= saveFile.get_value("Save state", "spawnPosY")
 	
