7161c499181ecc18cd584943a1990604c2475d6e
Damian Koch <Damian Koch@localhost>
2018-12-06 11:08:25 +0100

    added other checkpoiint/goal/spawnpoint assets + particle effects for spawnpoint/goal

--- Camera/Camera.gd
+++ Camera/Camera.gd
@@ -6,10 +6,7 @@ onready var heartImgWidth = hpFull.region_rect.size.x / 5
 
 #speed must be in [0, 1], cam won't move at 0
 export var camSpeed = 0.08
-#export var upDownSpeed = 0.05
 
-#distance camera can look up/down on pressing up/down
-#export var upDownAmount = 100
 
 func _ready():
 	if global.player != null:
@@ -27,18 +24,9 @@ func _ready():
 		updateNPCsaved()
 
 func _physics_process(delta):
-	#move camera to playerposition at certain speed, can move camera up/down while not climbing
-	#can no longer move camera up/down
+	#move camera to playerposition at certain speed
 	if global.player != null:
 		global_position.x = lerp(global_position.x, global.player.global_position.x, camSpeed)
-#		if global.player.state != global.player.climb:
-#			if Input.is_action_pressed("ui_down"):
-#				global_position.y = lerp(global_position.y, global.player.global_position.y + upDownAmount, upDownSpeed)
-#			elif Input.is_action_pressed("ui_up"):
-#				global_position.y = lerp(global_position.y, global.player.global_position.y - upDownAmount, upDownSpeed)
-#			else:
-#				global_position.y = lerp(global_position.y, global.player.global_position.y, upDownSpeed)
-#		else:
 		global_position.y = lerp(global_position.y, global.player.global_position.y, camSpeed)
 		
 				
