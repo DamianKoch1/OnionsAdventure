0227cebb33816c011670302c95e03d530138cfc9
Damian Koch <Damian Koch@localhost>
2018-12-06 12:02:39 +0100

    added camera offset, added new background assets

--- Camera/Camera.gd
+++ Camera/Camera.gd
@@ -7,6 +7,8 @@ onready var heartImgWidth = hpFull.region_rect.size.x / 5
 #speed must be in [0, 1], cam won't move at 0
 export var camSpeed = 0.08
 
+export var offsetX = 100
+export var offsetY = 60
 
 func _ready():
 	if global.player != null:
@@ -24,10 +26,12 @@ func _ready():
 		updateNPCsaved()
 
 func _physics_process(delta):
-	#move camera to playerposition at certain speed
+	#move camera to playerposition + offset at certain speed
+	var targetPosX = global.player.global_position.x + offsetX
+	var targetPosY = global.player.global_position.y - offsetY
 	if global.player != null:
-		global_position.x = lerp(global_position.x, global.player.global_position.x, camSpeed)
-		global_position.y = lerp(global_position.y, global.player.global_position.y, camSpeed)
+		global_position.x = lerp(global_position.x, targetPosX, camSpeed)
+		global_position.y = lerp(global_position.y, targetPosY, camSpeed)
 		
 				
 			
