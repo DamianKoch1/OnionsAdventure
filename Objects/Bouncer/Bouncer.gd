2efa7f8789ce17db7e136db1d812cea103b9e29b
Damian Koch <Damian Koch@localhost>
2018-12-13 11:37:17 +0100

    added bounce animation for bounce shroom

--- Objects/Bouncer/Bouncer.gd
+++ Objects/Bouncer/Bouncer.gd
@@ -3,8 +3,11 @@ extends Area2D
 export var maxBounceStr = 900
 export var bounceIncrease = 5
 
+onready var anim = $AnimationPlayer
+
 #make player bounce depending on current down motion, caps at maxBounceStrength
 func _on_Bouncer_body_entered(body):
 	if body == global.player:
 		if body.motion.y > 0:
 			body.bounce(min(body.motion.y+bounceIncrease*body.gravity, maxBounceStr))
+			anim.play("bounce")
