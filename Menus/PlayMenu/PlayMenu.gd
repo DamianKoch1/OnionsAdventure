b05f02df7ab96e4d84d83250ee8c63365f1e6159
Damian Koch <Damian Koch@localhost>
2019-01-07 15:23:55 +0100

    used autoload for ui select sound, volume no longer effected by position

--- Menus/PlayMenu/PlayMenu.gd
+++ Menus/PlayMenu/PlayMenu.gd
@@ -4,27 +4,27 @@ onready var levelSelector = preload("res://Menus/LevelSelector/LevelSelector.tsc
 onready var mainMenu = preload("res://Menus/MainMenu/MainMenu.tscn")
 
 func _on_ContinueButton_pressed():
-	$UISelect.playing = true
+	UISelect.playing = true
 	SaveGame.loadGame()
 	SaveGame.loadPlayerState = true
 
 func _on_LevelSelectorButton_pressed():
-	$UISelect.playing = true
+	UISelect.playing = true
 	get_tree().change_scene_to(levelSelector)
 
 
 func _on_OptionsButton_pressed():
-	$UISelect.playing = true
+	UISelect.playing = true
 	$OptionsOverlay.show()
 
 
 func _on_BackButton_pressed():
-	$UISelect.playing = true
+	UISelect.playing = true
 	get_tree().change_scene_to(mainMenu)
 
 
 func _on_NewGameButton_pressed():
-	$UISelect.playing = true
+	UISelect.playing = true
 	global.newGame = true
 	get_tree().change_scene("res://Menus/StoryPanels/NewGamePanels/NewGamePanels.tscn")
 
