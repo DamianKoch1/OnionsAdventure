a4fb8b07f7a5b7f5b43ea00cfe64f9f2d4274050
Damian Koch <Damian Koch@localhost>
2019-01-08 12:31:38 +0100

    added: -sounds for rope swinging         -respawning         -activating checkpoint         -moth flying -main menu music
    -adjusted rate at which sliders change volume (sound was barely audible at even half value)

--- Menus/OptionsOverlay/OptionsOverlay.gd
+++ Menus/OptionsOverlay/OptionsOverlay.gd
@@ -46,12 +46,12 @@ func _on_DiffSlider_value_changed(value):
 	setDiffDescr()
 
 func _on_SFXSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SoundEffects"), value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SoundEffects"), sqrt(value*sfxSlider.max_value) - 100)
 
 
 func _on_MusicSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), sqrt(value*musicSlider.max_value) - 100)
 
 
 func _on_MasterSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), sqrt(value*masterSlider.max_value) - 100)
