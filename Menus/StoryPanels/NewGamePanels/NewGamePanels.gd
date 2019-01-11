94282aa2b61aecdb3f288b6275e0cd84b354c265
Damian Koch <Damian Koch@localhost>
2019-01-11 10:45:56 +0100

    can now press spacebar to progress through story panels, removed old placeholders

--- Menus/StoryPanels/NewGamePanels/NewGamePanels.gd
+++ Menus/StoryPanels/NewGamePanels/NewGamePanels.gd
@@ -4,32 +4,53 @@ onready var anim = $AnimationPlayer
 
 onready var lv1 = preload("res://Levels/Level 1.tscn")
 
+export var atPanel = 1
+
 func _ready():
 	MenuMusic.fadeOut(2)
 	$BGMPlayer.fadeIn(2)
-	
+	atPanel = 1
+
+func _process(delta):
+	if Input.is_action_just_pressed("jump") && anim.is_playing() == false:
+		match atPanel:
+			1:
+				$pageTurn.playRandomPitch()
+				anim.play("1to2")
+			2:
+				$pageTurn.playRandomPitch()
+				anim.play("2to3")
+			3:
+				$pageTurn.playRandomPitch()
+				atPanel = 4
+				anim.play("3")
 	
 func loadLv1():
 	get_tree().change_scene_to(lv1)
 
 
 func _on_PanelButton1_pressed():
-	$pageTurn.playRandomPitch()
-	anim.play("1to2")
+	if anim.is_playing() == false:
+		$pageTurn.playRandomPitch()
+		anim.play("1to2")
 
 
 func _on_PanelButton2_pressed():
-	$pageTurn.playRandomPitch()
-	anim.play("2to3")
+	if anim.is_playing() == false:
+		$pageTurn.playRandomPitch()
+		anim.play("2to3")
 
 
 func _on_PanelButton3_pressed():
-	$pageTurn.playRandomPitch()
-	anim.play("3")
-	$BGMPlayer.fadeOut()
+	if anim.is_playing() == false:
+		atPanel = 4
+		$pageTurn.playRandomPitch()
+		anim.play("3")
+		$BGMPlayer.fadeOut()
 
 
 func _on_SkipButton_pressed():
+	atPanel = 4
 	if has_node("PanelButton1"):
 		$PanelButton1.disabled = true
 		$PanelButton1.hide()
