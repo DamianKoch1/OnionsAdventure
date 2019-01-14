6153433e147f58c0d9b4cbd2e53b2309d3cc4ac1
Damian Koch <Damian Koch@localhost>
2019-01-14 11:12:03 +0100

    prevented camera crash without player, started working on making spiderbuddy a hud element

--- Objects/interactable NPCs/Spiderbuddy/Spiderbuddy.gd
+++ Objects/interactable NPCs/Spiderbuddy/Spiderbuddy.gd
@@ -1,17 +1,18 @@
 extends Node2D
 
 onready var anim = $AnimationPlayer
+onready var label = $Speechbubble/Label
 
 #is hanging down?
 export var down = false
 
 
-func _on_upTrigger_body_entered(body):
-	if body.is_in_group("Player") && down == true && anim.is_playing() == false:
-		anim.play("hide")
-
-
-#on area exit isnt working properly due to player constantly changing parents
-func _on_downTrigger_body_entered(body):
-	if body.is_in_group("Player") && down == false && anim.is_playing() == false:
+func show(text):
+	if down == false && anim.is_playing() == false:
+		label.text = text
 		anim.play("show")
+
+
+func hide():
+	if down == true && anim.is_playing() == false:
+		anim.play("hide")
