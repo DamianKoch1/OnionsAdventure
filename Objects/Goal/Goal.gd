434a54958b1f266b8574aed574cf9e4d8ac2a95a
Damian Koch <Damian Koch@localhost>
2019-01-15 14:45:01 +0100

    added transition between levels, removed no longer working debug immunity

--- Objects/Goal/Goal.gd
+++ Objects/Goal/Goal.gd
@@ -1,8 +1,14 @@
 extends Area2D
 
+onready var panel = $CanvasLayer/Panel
+onready var anim = $AnimationPlayer
+
+func _ready():
+	anim.play("start")
+
 func _on_Goal_body_entered(body):
-	if body.is_in_group("Player"):
-			loadNextLevel()
+	if body.is_in_group("Player") && anim.is_playing() == false:
+			anim.play("end")
 
 func loadNextLevel():
 	global.currLevelId += 1
