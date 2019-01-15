243b303ff240139163655a557d144fe80871e9b4
Damian Koch <Damian Koch@localhost>
2019-01-15 15:43:16 +0100

    player loses control if touching goal

--- Objects/Goal/Goal.gd
+++ Objects/Goal/Goal.gd
@@ -9,8 +9,10 @@ func _ready():
 func _on_Goal_body_entered(body):
 	if body.is_in_group("Player") && anim.is_playing() == false:
 			anim.play("end")
+			body.state = body.noControl
 
 func loadNextLevel():
 	global.currLevelId += 1
 	var path = "Levels/Level " + str(global.currLevelId) + ".tscn"
+	print(path)
 	get_tree().change_scene(path)
