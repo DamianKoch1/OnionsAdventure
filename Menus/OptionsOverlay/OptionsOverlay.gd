fa65425ae4627dd174f8b4e568216a58294faf44
Damian Koch <Damian Koch@localhost>
2019-01-16 11:05:41 +0100

    volume now saves on change, removed global.gd

--- Menus/OptionsOverlay/OptionsOverlay.gd
+++ Menus/OptionsOverlay/OptionsOverlay.gd
@@ -1,20 +1,36 @@
 extends Container
 
 
+var savePath = "user://settings.cfg"
+var saveFile
+
 onready var masterSlider = $Volume/Master/MasterSlider
 onready var musicSlider = $Volume/Music/MusicSlider
 onready var sfxSlider = $Volume/SFX/SFXSlider
 
+#volume slider values
+var masterVol
+var musicVol
+var sfxVol
+
 
 func _ready():
-	if global.masterVol != null:
-		masterSlider.value = global.masterVol
-	if global.musicVol != null:
-		musicSlider.value = global.musicVol
-	if global.sfxVol != null:
-		sfxSlider.value = global.sfxVol
+	saveFile = ConfigFile.new()
+	loadVolume()
 	hide()
 
+func saveVolume():
+	saveFile.set_value("Volume", "master", masterSlider.value) 
+	saveFile.set_value("Volume", "music", musicSlider.value) 
+	saveFile.set_value("Volume", "sfx", sfxSlider.value) 
+	saveFile.save(savePath)
+
+func loadVolume():
+	if File.new().file_exists(savePath):
+		saveFile.load(savePath)
+		masterSlider.value = saveFile.get_value("Volume", "master")
+		musicSlider.value = saveFile.get_value("Volume", "music")
+		sfxSlider.value = saveFile.get_value("Volume", "sfx")
 
 func _on_BackButton_pressed():
 	UISelect.playing = true
@@ -22,12 +38,12 @@ func _on_BackButton_pressed():
 
 func _on_SFXSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SoundEffects"), sqrt(value*sfxSlider.max_value) - 100)
-	global.sfxVol = value
+	saveVolume()
 
 func _on_MusicSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), sqrt(value*musicSlider.max_value) - 100)
-	global.musicVol = value
+	saveVolume()
 
 func _on_MasterSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), sqrt(value*masterSlider.max_value) - 100)
-	global.masterVol = value
+	saveVolume()
