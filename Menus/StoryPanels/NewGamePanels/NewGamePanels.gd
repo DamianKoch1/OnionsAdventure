f7f698c865ecaa063f25e47e1e3144dd7dfa7684
Damian Koch <Damian Koch@localhost>
2019-01-29 15:13:46 +0100

    new game story panel buttons are now more object oriented

--- Menus/StoryPanels/NewGamePanels/NewGamePanels.gd
+++ Menus/StoryPanels/NewGamePanels/NewGamePanels.gd
@@ -1,63 +1,23 @@
 extends Node
 
-onready var anim = $AnimationPlayer
-
 onready var lv1 = preload("res://Levels/Level 1.tscn")
 
-export var atPanel = 1
+onready var skipped = false
 
 func _ready():
 	MenuMusic.fadeOut(2)
 	$BGMPlayer.fadeIn(2)
-	atPanel = 1
 
-func _process(delta):
-	if Input.is_action_just_pressed("jump") && anim.is_playing() == false:
-		match atPanel:
-			1:
-				$pageTurn.playRandomPitch()
-				anim.play("1to2")
-			2:
-				$pageTurn.playRandomPitch()
-				anim.play("2to3")
-			3:
-				$pageTurn.playRandomPitch()
-				atPanel = 4
-				anim.play("3")
-	
+
 func loadLv1():
 	get_tree().change_scene_to(lv1)
 
-
-func _on_PanelButton1_pressed():
-	if anim.is_playing() == false:
-		$pageTurn.playRandomPitch()
-		anim.play("1to2")
-
-
-func _on_PanelButton2_pressed():
-	if anim.is_playing() == false:
-		$pageTurn.playRandomPitch()
-		anim.play("2to3")
-
-
-func _on_PanelButton3_pressed():
-	if anim.is_playing() == false:
-		atPanel = 4
-		$pageTurn.playRandomPitch()
-		anim.play("3")
-		$BGMPlayer.fadeOut()
-
-
 func _on_SkipButton_pressed():
-	atPanel = 4
-	if has_node("PanelButton1"):
-		$PanelButton1.disabled = true
-		$PanelButton1.hide()
-	if has_node("PanelButton2"):
-		$PanelButton2.disabled = true
-		$PanelButton2.hide()
-	UISelect.playing = true
-	anim.play("3")
-	$BGMPlayer.fadeOut()
+	if skipped != true:
+		skipped = true
+		UISelect.playing = true
+		$BGMPlayer.fadeOut()
+		loadLv1()
 	
+func _on_PanelButton3_animFinished():
+	loadLv1()
--- Menus/StoryPanels/NewGamePanels/PanelButton.gd
+++ Menus/StoryPanels/NewGamePanels/PanelButton.gd
@@ -1,4 +1,15 @@
 extends TextureButton
 
+onready var anim = $AnimationPlayer
+
+signal animFinished
+
 func delete():
+	emit_signal("animFinished")
 	queue_free()
+
+
+func _on_PanelButton_pressed():
+	if anim.is_playing() != true:
+		$pageTurn.playRandomPitch()
+		anim.play("nextPanel")
