b8f9407ed6b73a06fbfca090a1fc46f674530ac8
Damian Koch <Damian Koch@localhost>
2019-01-30 11:14:11 +0100

    muddy sprites now flip if changing direction

--- Enemies/Enemy/Enemy.gd
+++ Enemies/Enemy/Enemy.gd
@@ -34,7 +34,11 @@ func _unique_process(delta):
 	if pathLooped == false:
 		if i >= 20*PI:
 			i = 0
+		#graph alternates between 0 and 1 every pi steps
 		pathfollow.unit_offset = acos(cos(i)) / acos(cos(PI))
+		#graph jumps fron 1 to -1 and back every PI steps
+		$EnemyPath/Enemy/Muddy.scale.x = sin(i)/(sqrt(1-(cos(i)*cos(i))))
+		
 	#move from a to a if on a closed path
 	elif pathLooped == true:
 		if i >= 20:
