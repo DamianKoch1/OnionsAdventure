44dd015ac8ceffbe092373210aef34012984156c
Damian Koch <Damian Koch@localhost>
2019-02-06 11:10:46 +0100

    spiderbuddy works properly, each trigger now has 15s cooldown

--- Objects/interactable NPCs/Spiderbuddy/SpiderbuddyTrigger.gd
+++ Objects/interactable NPCs/Spiderbuddy/SpiderbuddyTrigger.gd
@@ -4,10 +4,15 @@ extends Node2D
 export var text = "insert text"
 export var spiderCooldown = 15
 
-onready
+onready var cd = 0
 
 signal triggered
 
+func _process(delta):
+	cd = max(cd-delta, 0)
+
 func _on_Area2D_body_entered(body):
-	if body.is_in_group("Player"):
+	print(cd)
+	if body.is_in_group("Player") && cd == 0:
 		emit_signal("triggered", text)
+		cd = spiderCooldown
