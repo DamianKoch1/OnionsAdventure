c19a254d2469f641620447265833e53c554f8b67
Damian Koch <Damian Koch@localhost>
2019-02-11 16:35:47 +0100

    fixed yes/no overlay completely removing button focus, made fire salamander trigger bigger

--- Menus/MainMenu/MainMenu.gd
+++ Menus/MainMenu/MainMenu.gd
@@ -7,7 +7,9 @@ onready var extrasMenu = preload("res://Menus/ExtrasMenu/ExtrasMenu.tscn")
 
 func _ready():
 	$YesNoOverlayQuit.connect("yesPressed", self, "quit")
+	$YesNoOverlayQuit.connect("noPressed", self, "cancel")
 	$YesNoOverlayQuit.hide()
+	$PlayButton.grab_focus()
 	if MenuMusic.playing == false:
 		MenuMusic.fadeIn()
 	
@@ -18,8 +20,9 @@ func _on_CreditsButton_pressed():
 
 
 func _on_QuitButton_pressed():
-	UISelect.playing = true
-	$YesNoOverlayQuit.show()
+	if $YesNoOverlayQuit.visible == false:
+		UISelect.playing = true
+		$YesNoOverlayQuit.show()
 
 
 func _on_OptionsButton_pressed():
@@ -27,9 +30,11 @@ func _on_OptionsButton_pressed():
 	$OptionsOverlay.show()
 
 func quit():
-	UISelect.playing = true
 	get_tree().quit()
 
+func cancel():
+	$YesNoOverlayQuit.hide()
+	$PlayButton.grab_focus()
 
 func _on_PlayButton_pressed():
 	UISelect.playing = true
@@ -41,3 +46,9 @@ func _on_ExtrasButton_pressed():
 	UISelect.playing = true
 	get_tree().change_scene_to(extrasMenu)
 	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		if $YesNoOverlayQuit.visible == false:
+			_on_QuitButton_pressed()
+		else:
+			cancel()
