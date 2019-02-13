ae2213894548735f5e521c0845762fc0b535af43
Damian Koch <Damian Koch@localhost>
2019-02-13 13:12:17 +0100

    crash fix

--- Menus/YesNoOverlay/YesNoOverlay.gd
+++ Menus/YesNoOverlay/YesNoOverlay.gd
@@ -9,10 +9,8 @@ func _ready():
 	hide()
 
 func _on_NoButton_pressed():
-	UISelect.playing = true
 	emit_signal("noPressed")
 
 func _on_YesButton_pressed():
-	UISelect.playing = true
 	emit_signal("yesPressed")
 
