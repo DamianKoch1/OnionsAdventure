e91c53f8e3be49a77f216a1293179b46f49d4b6a
Damian Koch <Damian Koch@localhost>
2019-02-15 15:38:27 +0100

    added fades for pause menu

--- Menus/PauseMenu/PauseMenu.gd
+++ Menus/PauseMenu/PauseMenu.gd
@@ -4,10 +4,12 @@ onready var paused = false
 
 onready var mainMenu = preload("res://Menus/MainMenu/MainMenu.tscn")
 
+onready var fade = $Fade
+
 func _ready():
-	$YesNoOverlayMainMenu.connect("yesPressed", self, "loadMainMenu")
+	$YesNoOverlayMainMenu.connect("yesPressed", self, "mainMenuFade")
 	$YesNoOverlayMainMenu.connect("noPressed", self, "cancel")
-	$YesNoOverlayRestart.connect("yesPressed", self, "restart")
+	$YesNoOverlayRestart.connect("yesPressed", self, "restartFade")
 	$YesNoOverlayRestart.connect("noPressed", self, "cancel")
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	
@@ -45,21 +47,40 @@ func _on_RestartButton_pressed():
 	$YesNoOverlayRestart/NoButton.grab_focus()
 
 func _on_OptionsButton_pressed():
+	fade.oneshot(self, "options", 2)
+
+func options():
 	$OptionsOverlay.show()
 	$OptionsOverlay/BackButton.grab_focus()
+	fade.fadeIn(2)
 
 func _on_MainMenuButton_pressed():
 	$YesNoOverlayMainMenu.show()
 	$YesNoOverlayMainMenu/NoButton.grab_focus()
 
-func loadMainMenu():
+func mainMenuFade():
 	get_tree().paused = false
+	fade.oneshot(self, "mainMenu")
+
+func mainMenu():
 	get_tree().change_scene_to(mainMenu)
+	
+func restartFade():
+	get_tree().paused = false
+	fade.oneshot(self, "restart")
 
 func restart():
-	get_tree().paused = false
 	get_tree().reload_current_scene()
 	
 func cancel():
 	$YesNoOverlayMainMenu.hide()
 	$YesNoOverlayRestart.hide()
+
+#options menu back button
+func _on_BackButton_pressed():
+	fade.oneshot(self, "back", 2) 
+
+func back():
+	$OptionsOverlay.hide()
+	fade.fadeIn(2)
+	$ResumeButton.grab_focus()
