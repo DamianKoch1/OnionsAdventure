7306ecdb2367267b23da80ddade07bd47c06e26e
Damian Koch <Damian Koch@localhost>
2019-02-15 15:19:39 +0100

    added fades for credits

--- Menus/MainMenu/CreditScreen.gd
+++ Menus/MainMenu/CreditScreen.gd
@@ -1,8 +1,11 @@
 extends Container
 
 onready var mainMenu = preload("res://Menus/MainMenu/MainMenu.tscn")
+onready var fade = $Fade
+
 
 func _ready():
+	fade.fadeIn()
 	$BackButton.grab_focus()
 
 func _process(delta):
@@ -10,5 +13,8 @@ func _process(delta):
 		_on_BackButton_pressed()
 
 func _on_BackButton_pressed():
+	fade.oneshot(self, "mainMenu", 2)
+	
+func mainMenu():
 	get_tree().change_scene_to(mainMenu)
 
