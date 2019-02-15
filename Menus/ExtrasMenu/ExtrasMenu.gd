1048eb744bc6e1809280bd716a2f8791b6295284
Damian Koch <Damian Koch@localhost>
2019-02-15 14:53:57 +0100

    added fades and page transitions for gallery

--- Menus/ExtrasMenu/ExtrasMenu.gd
+++ Menus/ExtrasMenu/ExtrasMenu.gd
@@ -4,55 +4,58 @@ onready var mainMenu = preload("res://Menus/MainMenu/MainMenu.tscn")
 onready var dandelionCounter = $Dandelions/Label
 onready var NPCCounter = $TrappedNPCs/Label
 onready var atPage = 1
+onready var anim = $AnimationPlayer
+onready var page = $Page
+onready var fade = $Fade
 
 
 func _ready():
+	fade.fadeIn()
 	dandelionCounter.text = str(SaveGame.dandelions) + "/200"
 	NPCCounter.text = str(SaveGame.trappedNPCs) + "/12"
-	$Page1.show()
-	$Page2.hide()
-	$Page3.hide()
-	$Page4.hide()
 	$BackButton.hide()
 	$NextButton.grab_focus()
+
+	
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		_on_MainMenuButton_pressed()
 
 func _on_BackButton_pressed():
-	$NextButton.show()
-	if atPage == 2:
-		$Page1.show()
-		$Page2.hide()
-		atPage = 1
-		$BackButton.hide()
-		$NextButton.grab_focus()
-	elif atPage == 3:
-		$Page2.show()
-		$Page3.hide()
-		atPage = 2
-	elif atPage == 4:
-		$Page3.show()
-		$Page4.hide()
-		atPage = 3
+	if anim.is_playing() == false:
+		$NextButton.show()
+		if atPage == 2:
+			anim.play_backwards("1to2")
+			atPage = 1
+			$BackButton.hide()
+			$NextButton.grab_focus()
+		elif atPage == 3:
+			anim.play_backwards("2to3")
+			atPage = 2
+		elif atPage == 4:
+			anim.play_backwards("3to4")
+			atPage = 3
+		page.text = "Page " + str(atPage) + "/4"
 
 func _on_NextButton_pressed():
-	$BackButton.show()
-	if atPage == 1:
-		$Page1.hide()
-		$Page2.show()
-		atPage = 2
-	elif atPage == 2:
-		$Page2.hide()
-		$Page3.show()
-		atPage = 3
-	elif atPage == 3:
-		$Page3.hide()
-		$Page4.show()
-		atPage = 4
-		$NextButton.hide()
-		$BackButton.grab_focus()
+	if anim.is_playing() == false:
+		$BackButton.show()
+		if atPage == 1:
+			anim.play("1to2")
+			atPage = 2
+		elif atPage == 2:
+			anim.play("2to3")
+			atPage = 3
+		elif atPage == 3:
+			anim.play("3to4")
+			atPage = 4
+			$NextButton.hide()
+			$BackButton.grab_focus()
+		page.text = "Page " + str(atPage) + "/4"
 
 func _on_MainMenuButton_pressed():
+	fade.oneshot(self, "mainMenu", 2)
+
+func mainMenu():
 	get_tree().change_scene_to(mainMenu)
