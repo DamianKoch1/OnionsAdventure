d592f38e25e7758ab8eb377c517312d3e4716711
Damian Koch <Damian Koch@localhost>
2019-02-21 13:16:09 +0100

    story panels show when using level selector

--- Menus/LevelSelector/LevelButton/LevelButton.gd
+++ Menus/LevelSelector/LevelButton/LevelButton.gd
@@ -2,21 +2,22 @@ extends TextureButton
 
 export var levelId = 1
 onready var fade = $CanvasLayer/Fade
+onready var transitionPanels = preload("res://Menus/StoryPanels/TransitionPanels/TransitionPanels.tscn")
+onready var newGamePanels = preload("res://Menus/StoryPanels/NewGamePanels/NewGamePanels.tscn")
+var scene
 
-
-func loadLevel(id):
-	SaveGame.currLevelId = id
-	var path = "Levels/Level " + str(SaveGame.currLevelId) + ".tscn"
-	get_tree().change_scene(path)
+func loadScene():
+	if levelId == 1:
+		scene = newGamePanels
+	else:
+		scene = transitionPanels
+	SaveGame.currLevelId = levelId-1
+	get_tree().change_scene_to(scene)
 
 
 func _on_LevelButton_pressed():
 	$UISelect.playing = true
-	fade.oneshot(self, "loadLvl")
-
-#no arguments needed
-func loadLvl():
-	loadLevel(levelId)
+	fade.oneshot(self, "loadScene")
 
 
 func _on_LevelButton_mouse_entered():
