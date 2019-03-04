16fefc51e08dea9a4ddffaab297818f4ebc3b42a
Damian Koch <Damian Koch@localhost>
2019-03-04 14:46:08 +0100

    reworked ladder attaching cooldown

--- Objects/Ladder/Ladder.gd
+++ Objects/Ladder/Ladder.gd
@@ -1,26 +1,25 @@
 extends Area2D
 
 onready var playerAttached = false
-var attachCD = 0
 
 var player
 
 #player can jump off if attached
 func _physics_process(delta):
-	attachCD = max(attachCD - delta, 0)
 	if Input.is_action_just_pressed("jump") && playerAttached == true:
 		player.jump(player.jumpheight)
-		attachCD = 1
+		player.attachCD = 1
 		playerAttached = false
 		player = null
 
 #give player ladder to attach to and make him climb on contact
 func _on_Ladder_body_entered(body):
-	if body.is_in_group("Player") && attachCD == 0:
-		body.rope = self
-		body.state = body.climb
-		playerAttached = true
-		player = body
+	if body.is_in_group("Player"):
+		if body.attachCD == 0:
+			body.rope = self
+			body.state = body.climb
+			playerAttached = true
+			player = body
 
 #detach player if he exits body
 func _on_Ladder_body_exited(body):
