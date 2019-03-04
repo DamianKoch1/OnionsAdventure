e0d53eb7f915f7b20630bfb1ea50b21a58e8c558
Damian Koch <Damian Koch@localhost>
2019-03-04 15:49:49 +0100

    changed concept unlocked popup

--- Camera/Camera.gd
+++ Camera/Camera.gd
@@ -5,7 +5,6 @@ onready var npcFull = $CanvasLayer/NPCs/NPCsFull
 onready var npcEmpty = $CanvasLayer/NPCs/NPCsEmpty
 onready var npcImgWidth  = npcFull.region_rect.size.x / 3
 
-var popupShown = false
 
 #is assigned by start module
 var player
@@ -32,12 +31,12 @@ func connectSignals():
 
 func updateNPCsaved():
 	npcFull.region_rect.size.x = (3 - get_tree().get_nodes_in_group("trappedNPCs").size()) * npcImgWidth
-	if SaveGame.dandelions >= 5 && SaveGame.trappedNPCs >= 1 && popupShown == false:
+	if SaveGame.dandelions == 5 && SaveGame.trappedNPCs == 1:
 		$CanvasLayer/ConceptUnlockPopup.popup()
 		popupShown = true
 
 func updateCollectables():
 	dandelionCounter.text = str(50-get_tree().get_nodes_in_group("dandelions").size()) + "/50"
-	if SaveGame.dandelions >= 5 && SaveGame.trappedNPCs >= 1 && popupShown == false:
+	if SaveGame.dandelions == 5 && SaveGame.trappedNPCs == 1 && popupShown == false:
 		$CanvasLayer/ConceptUnlockPopup.popup()
 		popupShown = true
